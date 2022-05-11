87a90918913264e53df210b172d2df57dc791fcc
Emeka Mamah <mamahemeka84@gmail.com>
2022-05-11 15:29:51 +0100

    update

---
+++ pages/transactions/withdrawal.tsx
@@ -0,0 +1,13 @@
+import {NextPage} from "next";
+import DashboardLayout from "../../components/Layouts/dashboard";
+
+
+const Withdrawal: NextPage = () => {
+  return (
+      <DashboardLayout>
+
+      </DashboardLayout>
+  )
+}
+
+export default Withdrawal
