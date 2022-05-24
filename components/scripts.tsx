6ee082f0730bdfee9fbee8831aba60931a0559f0
Lawrence Nwoko Ndubueze <nwokolawrence6@gmail.com>
2022-05-24 02:38:17 +0100

    Fixed next js Hydration bug

--- components/scripts.tsx
+++ components/scripts.tsx
@@ -1,13 +1,17 @@
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
 import Script from "next/script";
 
 
-const ExternalScripts: FC = () => {
+const ExternalScripts: () => false | JSX.Element = () => {
+  const [loaded, setLoaded] = useState(false);
+  useEffect(()=> {
+    setLoaded(true)
+  }, [])
     return (
-        <>
+        loaded &&<>
             <Script src={'/assets/js/bundle.js?ver=3.0.2'} strategy={'afterInteractive'}/>
-            <Script src={'/assets/js/scripts.js?ver=3.0.2'} strategy={'beforeInteractive'}/>
-            <Script src={'/assets/js/charts/chart-crypto.js?ver=3.0.2'} strategy={'beforeInteractive'}/>
+            {/*<Script src={'/assets/js/scripts.js?ver=3.0.2'}  strategy={'afterInteractive'}/>*/}
+            {/*<Script src={'/assets/js/charts/chart-crypto.js?ver=3.0.2'} strategy={'afterInteractive'}/>*/}
         </>
     )
 }
